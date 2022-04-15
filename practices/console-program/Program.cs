f4e0d085c859a14584d94d0e818b8dec9aaf96dc
nurayklc <nuraykilic0101@gmail.com>
2022-04-15 23:38:03 +0300

    Add console program exercise

---
+++ practices/console-program/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter name");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter surname");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Helloo " + name + " " + surname);
