4a4f86a85c29f72f9556d12b5f3ed8b8acb9880a
nurayklc <nuraykilic0101@gmail.com>
2022-04-18 23:25:56 +0300

    Complete if-else lesson

---
+++ practices/conditional-statements/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int time = DateTime.Now.Hour;
+
+if (time <= 11 && time >= 6)
+{
+    Console.WriteLine("Günaydın...");
+}
+else if (time <= 18)
+{
+    Console.WriteLine("İyi günler...");
+}
+else
+{
+    Console.WriteLine("İyi geceler...");
+}
+
+string result = time <= 18 ? "İyi günler..." : (time <= 11 && time >= 6) ? "Günaydın..." : "İyi geceler...";
+Console.WriteLine(result);
