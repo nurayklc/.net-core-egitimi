eb61c6ea0608532d47a26fc9c247dec3514639c0
nurayklc <nuraykilic0101@gmail.com>
2022-04-19 02:14:16 +0300

    Complete switch-case lesson

--- practices/conditional-statements/Program.cs
+++ practices/conditional-statements/Program.cs
@@ -17,3 +17,61 @@ else
 
 string result = time <= 18 ? "İyi günler..." : (time <= 11 && time >= 6) ? "Günaydın..." : "İyi geceler...";
 Console.WriteLine(result);
+
+// Switch Case
+
+int month = DateTime.Now.Month;
+
+// Expression
+switch (month)
+{
+    case 1:
+        Console.WriteLine("Ocak Ayı");
+        break;
+    case 2:
+        Console.WriteLine("Şubat Ayı");
+        break;
+    case 3:
+        Console.WriteLine("Mart Ayı");
+        break;
+    case 4:
+        Console.WriteLine("Nisan Ayı");
+        break;
+    case 5:
+        Console.WriteLine("Mayıs Ayı");
+        break;
+
+    default:
+        Console.WriteLine("Yanlış veri girişi!");
+        break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış Mevsimi");
+        break;
+    
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlkbahar Mevsimi");
+        break;
+
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz Mevsimi");
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("İlkbahar Mevsimi");
+        break;   
+
+    default:
+        break;
+}
