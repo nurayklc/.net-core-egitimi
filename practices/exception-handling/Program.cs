5bffc2adbda7da7c438698d943d14f7cae64644a
nurayklc <nuraykilic0101@gmail.com>
2022-04-18 21:10:45 +0300

    Complete exception handling lesson

---
+++ practices/exception-handling/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    Console.WriteLine("Please enter a number : ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Number : " + sayi);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error : " + ex.Message.ToString());
+    Console.WriteLine("Please enter a number !");
+}
+finally
+{
+    Console.WriteLine("Have a good day..");
+}
+
+// Exercise 2:
+try
+{
+    //int a = int.Parse(null);
+    int b = int.Parse("sa");
+    int c = int.Parse("12345678910");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Error : This value is empty. " + ex);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Error : This value was not in a corrcet format." + ex);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Error : Çok yüsek veya çok düşük değer girdiniz " + ex);
+}
