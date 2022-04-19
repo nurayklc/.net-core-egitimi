427dc6dc9f40f52c688a7dd2140eedd71a316475
nurayklc <nuraykilic0101@gmail.com>
2022-04-19 17:10:32 +0300

    Completefor loop and break-continue lesson

---
+++ practices/loops/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lütfen bir sayı giriniz: ");
+int count = int.Parse(Console.ReadLine());
+for (int i = 1; i <= count; i++)
+{
+    if (i % 2 == 1)
+        Console.WriteLine(i);
+}
+
+int oddSum = 0;
+int evenSum = 0;
+for (int i = 1; i <= 1000; i++)
+{
+    if (i % 2 == 1)
+        oddSum += i;
+    else
+        evenSum += i;
+}
+
+Console.WriteLine("Tek Toplam " + oddSum);
+Console.WriteLine("Çift Toplam " + evenSum);
+
+//break, continue
+for (int i = 1; i < 10; i++)
+{
+    if (i == 4)
+        continue;
+    Console.WriteLine(i);
+}
