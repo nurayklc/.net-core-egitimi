1f5d7110f0a86f017389a04fd8f9fc8c941d75d6
nurayklc <nuraykilic0101@gmail.com>
2022-04-20 23:53:30 +0300

    Complete methods first lesson

---
+++ practices/methods/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static int sum(int param1, int param2){
+    return param1+param2;
+}
+
+// Call by Value 
+Console.WriteLine(sum(4,9));
+Console.WriteLine(sum(85,78));
+
+ExerciseMethods exerciseMethod = new ExerciseMethods();
+
+exerciseMethod.ConsoleWrite("Sum Method" + Convert.ToString(sum(23,56)));
+
+// Ref by Value 
+exerciseMethod.increaseAndSum(11,22);
+
+class ExerciseMethods {
+
+    public void ConsoleWrite(string data){
+        Console.WriteLine(data);
+    }
+
+    // Ref by Value
+    public int increaseAndSum(ref int param1, ref int param2){
+        param1 +=1;
+        param2 +=1;
+        return param1 + param2;
+    }
+
+}
