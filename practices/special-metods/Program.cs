75a741a29619c747e974771538468aa4ac49c032
nurayklc <nuraykilic0101@gmail.com>
2022-04-22 00:12:43 +0300

    Complete math and datetime library

--- practices/special-metods/Program.cs
+++ practices/special-metods/Program.cs
@@ -50,8 +50,9 @@ Console.WriteLine(variable + variable2.PadLeft(30, '*'));
 Console.WriteLine(variable.PadRight(50, '-') + variable2);
 
 
-//Remove : Verilen indexteki değeri siler
+//Remove : Verilen indexteki değerden sonrasını siler
 Console.WriteLine(variable.Remove(10));
+// 5. indexten başlayarak 3 karakter siler
 Console.WriteLine(variable.Remove(5, 3));
 Console.WriteLine(variable.Remove(0, 1));
 
@@ -65,5 +66,67 @@ Console.WriteLine(variable.Replace(" ", "*"));
 Console.WriteLine(variable.Split(" ")[1]);
 
 
-//SubString
+//SubString : 4. indexten başlayarak stringin sonuna kadar tüm karakterleri getirir
 Console.WriteLine(variable.Substring(4));
+
+
+
+// DateTime Kütüphanesi
+
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.Now.Date);
+Console.WriteLine(DateTime.Now.Day);
+Console.WriteLine(DateTime.Now.Month);
+Console.WriteLine(DateTime.Now.Year);
+Console.WriteLine(DateTime.Now.Hour);
+Console.WriteLine(DateTime.Now.Minute);
+Console.WriteLine(DateTime.Now.Second);
+
+Console.WriteLine(DateTime.Now.DayOfWeek);
+Console.WriteLine(DateTime.Now.DayOfYear);
+
+Console.WriteLine(DateTime.Now.ToLongDateString());
+Console.WriteLine(DateTime.Now.ToShortDateString());
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+Console.WriteLine(DateTime.Now.AddDays(2));
+Console.WriteLine(DateTime.Now.AddHours(3));
+Console.WriteLine(DateTime.Now.AddSeconds(30));
+Console.WriteLine(DateTime.Now.AddMonths(5));
+Console.WriteLine(DateTime.Now.AddYears(10));
+Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+// DateTime Format
+Console.WriteLine(DateTime.Now.ToString("dd"));   // 16
+Console.WriteLine(DateTime.Now.ToString("ddd"));  // Sat
+Console.WriteLine(DateTime.Now.ToString("dddd")); // Saturday
+
+Console.WriteLine(DateTime.Now.ToString("MM"));   // 04
+Console.WriteLine(DateTime.Now.ToString("MMM"));  // Apr
+Console.WriteLine(DateTime.Now.ToString("MMMM")); // April
+
+Console.WriteLine(DateTime.Now.ToString("yy"));   // 22
+Console.WriteLine(DateTime.Now.ToString("yyyy"));  // 2022
+
+
+// Math Kütüphanesi
+Console.WriteLine(Math.Abs(-25)); // 25
+
+Console.WriteLine(Math.Sin(10)); // 25
+Console.WriteLine(Math.Cos(10)); // 25
+Console.WriteLine(Math.Tan(10)); // 25
+
+Console.WriteLine(Math.Ceiling(22.3)); // 23
+Console.WriteLine(Math.Round(22.3));   // 22
+Console.WriteLine(Math.Round(22.7));   // 23
+Console.WriteLine(Math.Floor(22.7));   // 22
+
+Console.WriteLine(Math.Max(2,6)); 
+Console.WriteLine(Math.Min(2,6)); 
+
+Console.WriteLine(Math.Pow(2,6)); 
+Console.WriteLine(Math.Sqrt(9)); 
+Console.WriteLine(Math.Log(9));  
+Console.WriteLine(Math.Exp(3));  
+Console.WriteLine(Math.Log10(10)); 
