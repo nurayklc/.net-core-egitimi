3821a5e251c2272c0a3e994e2f70bbc192e0d362
nurayklc <nuraykilic0101@gmail.com>
2022-04-26 00:54:17 +0300

    Completed first question in collections algorithm

--- practices/second-hmwork/Collections-First-Question/Program.cs
+++ practices/second-hmwork/Collections-First-Question/Program.cs
@@ -9,45 +9,57 @@ ArrayList notPrimeList = new ArrayList();
 // Girdi Alma
 Console.WriteLine("20 Adet Sayı Giriniz: ");
 for(int i = 0; i < 20; i++){
-    Console.WriteLine(i + ". Sayınızı Giriniz: ");
-    int n = Convert.ToInt32(Console.ReadLine());
-    bool result = n.isPositive();
-    bool result2 = n.isNumeric();
-    bool result3 = n.isPrime();
-    if( result && result2 && result3){
-        primeList.Add(n);
-    }
-    if( result && result2){
-        if(result3 == false){
-            notPrimeList.Add(n);
+    back:
+    try
+    {
+        Console.WriteLine(i+1 + ". Sayınızı Giriniz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if(!n.isPositive()){
+            Console.WriteLine("Not negative number!!");
+            goto back;
         }
+        bool result = n.isPrime();
+        if(result)
+            primeList.Add(n);
+        else
+            notPrimeList.Add(n);
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Not negative number and numeric number!!");
+        goto back;
     }
 }
 
 // Listeleme
 primeList.Sort();
+int primeMedium = 0;
+Console.WriteLine("-- Prime Number List --");
 foreach (var item in primeList)
 {
+    primeMedium += Convert.ToInt32(item);
     Console.WriteLine(item);
 }
+Console.WriteLine("Prime List Length: " + primeList.Count);
+Console.WriteLine("Prime List Medium: " + Convert.ToInt32(primeMedium / primeList.Count));
 
 notPrimeList.Sort();
+int notPrimeMedium = 0;
+Console.WriteLine("-- Not Prime Number List --");
 foreach (var item in notPrimeList)
 {
+    notPrimeMedium += Convert.ToInt32(item);
     Console.WriteLine(item);
 }
 
+Console.WriteLine("Not Prime List Length: " + notPrimeList.Count);
+Console.WriteLine("Not Prime List Medium: " + Convert.ToInt32(notPrimeMedium / notPrimeList.Count));
 
 
 // Extension Methods
 public static class Extension{
     public static bool isPositive(this int number){
         return number > 0;
-    }
-
-    public static bool isNumeric(this int number){
-        int temp = 0;
-        return int.TryParse(number, out temp); 
     }
 
     public static bool isPrime(this int number){
