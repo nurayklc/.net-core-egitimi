7941e9672da1085630400872a7f2c74e178bddd9
nurayklc <nuraykilic0101@gmail.com>
2022-04-26 02:47:48 +0300

    Completed third question in secon hmwork

---
+++ practices/second-hmwork/Collections-Third-Question/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// Third Question - Vowels Find
+
+List<char> vowels = new List<char>(){'a','e','i','o','u'};
+Console.WriteLine("Please enter a sentence!!");
+
+string sentence = Convert.ToString(Console.ReadLine());
+char [] arr = new char[sentence.Length];
+
+for (int i = 0; i < sentence.Length; i++)
+{
+    if(vowels.Contains(sentence[i])){
+        arr[i] = sentence[i];
+    }
+}
+
+// Vowels List
+foreach (var item in arr)
+{
+    Console.Write(item + ", ");
+}
