4134aeb30a98f13b9f671fff63df6214032d11b2
nurayklc <nuraykilic0101@gmail.com>
2022-04-27 17:56:10 +0300

    Complete enum lesson and section

---
+++ practices/classes/enum/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine(Days.Monday); 
+// Numeric karşılığına erişme
+Console.WriteLine((int)Days.Sunday); 
+
+int temperature = 32;
+
+if(temperature <= (int)Weather.Normal){
+    Console.WriteLine("Wait");
+}
+else if( temperature >= (int)Weather.Hot){
+    Console.WriteLine("Very Hot");
+}
+else if( temperature >= (int)Weather.Normal){
+    Console.WriteLine("Lets go outside");
+}
+
+
+enum Days {
+    //Monday = 1, 
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday, 
+    Friday,
+    Saturday,
+    Sunday
+}
+
+enum Weather {
+    Cold = 5,
+    Normal = 24,
+    Hot = 30,
+    VeryHot = 35,
+}
