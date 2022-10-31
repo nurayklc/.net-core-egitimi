3d8a240d3a3ef7680d8a07d4a961afd48c4d7fa5
nurayklc <nuraykilic0101@gmail.com>
2022-10-31 11:26:39 +0300

    Complete triangle drawing

---
+++ TriangleDrawing/TriangleDrawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TriangleDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen boyut sayısını giriniz: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            triangle(number);
+        }
+        static void triangle(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = i; j <= number; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
