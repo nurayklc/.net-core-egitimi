96e582b68a4d117e7fe93b129007982f09e2f140
nurayklc <nuraykilic0101@gmail.com>
2022-10-31 14:51:46 +0300

    Add string reverse

---
+++ CharReplace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CharReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle giriniz: ");
+            var str = Console.ReadLine();
+
+            var newStr = str.Split(" ");
+            for(int i= 0; i < newStr.Length; i++)
+            {
+                var result = Reverse(newStr[i]);
+                Console.Write(result + " ");
+            }
+        }
+
+        static string Reverse(string str)
+        {
+            var res = "";
+            for (int j = str.Length -1 ; j >= 0; j--)
+            {
+                res += str[j];
+            }
+            return res;
+        }
+    }
+}
