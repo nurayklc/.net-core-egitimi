26b72a86685121d39ba47f5e81d352c1cca870e7
nurayklc <nuraykilic0101@gmail.com>
2022-10-31 14:25:59 +0300

    Add remove char in string

---
+++ StringAndInt/StringAndInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringAndInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir kelime ve bir sayıyı aralarında virgül ile giriniz: ");
+            var str = Console.ReadLine();
+
+            RemoveChar(str);
+        }
+
+        static void RemoveChar(string str)
+        {
+            var newStr = str.Split(",");
+
+            var word = newStr[0].ToString();
+            var number = Convert.ToInt32(newStr[1]);
+
+            word = word.Remove(number, 1);
+
+            Console.WriteLine(word);
+        }
+    }
+}
